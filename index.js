f3fcddd4bb2e3d03b6ccca5ea6224130360e0071
Medina Obralija <medina.obralija@gmail.com>
2024-10-16 15:01:46 +0200

    add dependencies, improve redis client

--- index.js
+++ index.js
@@ -1,42 +1,78 @@
-const express = require('express');
-const app = express();
-const port = 3000;
-const redis = require('redis');
-const client = redis.createClient();
+import express from 'express';
+import { createClient } from 'redis';
+import dotenv from 'dotenv';
 
-client.connect().then(() => {
-	console.log('Connected to Redis...');
+dotenv.config();
+
+const app = express();
+const port = process.env.PORT || 3000;
+
+const client = createClient({ url: 'redis://localhost:6379' });
+
+async function connectRedis() {
+	try {
+		await client.connect();
+		console.log('Connected to Redis...');
+	} catch (err) {
+		console.error('Redis connection error:', err);
+	}
+}
+
+await connectRedis();
+
+client.on('error', (err) => {
+	console.error('Redis Client Error', err);
 });
 
+client.on('ready', () => {
+	console.log('Redis client connected and ready!');
+});
+
+client.on('end', () => {
+	console.log('Redis client disconnected');
+});
+
+const getAsync = client.get.bind(client);
+const setExAsync = client.setEx.bind(client);
+
+// Middleware for rate limiting
 const rateLimiter = async (req, res, next) => {
-	const userIP = req.ip;
-	const timeWindow = 60; // 1 minute
-	const maxRequests = 5;
-
 	try {
-		const record = await client.get(userIP);
-		const currentRequestCount = record ? parseInt(record) : 0;
+		if (!client.isOpen) {
+			console.error('Redis client is not connected');
+			return res.status(500).json({ message: 'Redis connection error' });
+		}
 
-		if (currentRequestCount >= maxRequests) {
-			res.status(429).send('Too many requests. Please try again later.');
+		const userIP = req.ip;
+		const timeWindow = process.env.TIME_WINDOW || 60;
+		const maxRequests = process.env.MAX_REQUESTS || 5;
+
+		const currentRequestCount = await getAsync(userIP);
+		const requestCount = currentRequestCount
+			? parseInt(currentRequestCount)
+			: 0;
+
+		if (requestCount >= maxRequests) {
+			console.log(`Rate limit exceeded for IP: ${userIP}`);
+			return res.status(429).json({
+				message: 'Too many requests. Please try again later.',
+			});
 		} else {
-			await client.setEx(
-				userIP,
-				timeWindow.toString(),
-				(currentRequestCount + 1).toString()
-			);
+			await setExAsync(userIP, timeWindow, (requestCount + 1).toString());
 			next();
 		}
-	} catch (err) {
-		console.error('Error in rateLimiter:', err);
-		res.status(500).send('Internal Server Error');
+	} catch (error) {
+		console.error('Error in rateLimiter:', error);
+		res.status(500).json({ message: 'Internal server error' });
 	}
 };
 
+// Route
 app.get('/', rateLimiter, (req, res) => {
 	res.send('Welcome, your request is within limit!');
 });
 
+// Listen for requests
 app.listen(port, () => {
 	console.log(`Server running on http://localhost:${port}`);
 });
